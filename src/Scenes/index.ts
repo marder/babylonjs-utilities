6277037dc2680c3a0c817c61c6d552dd5a5a031a
rammbulanz <iwashier@gmail.com>
2018-09-02 00:38:41 +0200

    Remvoed index files.

--- src/Scenes/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './ArenaInit'
-export * from './Arena'
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-// Import MixIns that edit BABYLONJS Classes
-// Use them with caution
-import './node-metadata-mixin'
-
-// Import and export the rest
-export * from './Cameras'
-export * from './Scenes'
